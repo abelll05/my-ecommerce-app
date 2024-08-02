04f7cdcaeba584d2e19c7e6f6415bdd6d5af73b9
abelll05 <127783359+abelll05@users.noreply.github.com>
2024-08-02 04:26:48 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
-// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { CartProvider } from './context/CartContext'; // Asegúrate de que la ruta sea correcta
+import { CartProvider } from './context/CartContext'; 
 import './index.css';
 
 ReactDOM.render(
